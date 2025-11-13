1d4d7332cf5ad434e3a1ad1c42cdcbb9969796b2
LmanTW <lmantw2009@gmail.com>
2025-11-13 16:13:59 +0800

    fix: use the production library name

--- src/core/Dekun.zig
+++ src/core/Dekun.zig
@@ -915,7 +915,7 @@ pub fn loadPythonBridge(self: *Dekun, padding: Console.Block.Padding) !Bridge {
     const library_path = result: {
         switch (builtin.os.tag) {
             .linux => {
-                const name = try std.fmt.allocPrint(self.allocator, "libpython{}.{}d.so.1.0", .{buinfo.cpython.major, buinfo.cpython.minor});
+                const name = try std.fmt.allocPrint(self.allocator, "libpython{}.{}.so.1.0", .{buinfo.cpython.major, buinfo.cpython.minor});
                 defer self.allocator.free(name);
 
                 break :result try std.fs.path.join(self.allocator, &.{self.root_path, "python-debug", "lib", name});
